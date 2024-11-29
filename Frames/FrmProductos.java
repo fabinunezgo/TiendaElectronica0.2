13a5ff72b729d828283ef6c9315b89d15d2167e0
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-11-28 21:16:58 -0600

    Correccion de conflictos

--- Frames/FrmProductos.java
+++ Frames/FrmProductos.java
@@ -1,10 +1,7 @@
 package Frames;
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 3ba5208 (Se hacen algunos cambios en los formularios)
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template

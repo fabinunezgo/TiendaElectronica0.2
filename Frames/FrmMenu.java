7aa925aa5be2593c1370ebf1e6d91e39b42e875d
Thyfa <thmartinezgo@est.utn.ac.cr>
2024-12-03 15:49:03 -0600

    d2

--- Frames/FrmMenu.java
+++ Frames/FrmMenu.java
@@ -106,8 +106,7 @@ public class FrmMenu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-       ;
-
+      
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
